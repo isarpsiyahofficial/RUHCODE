5ee5850d36b896e14fed3de12a63874b389bb7b5
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-18 11:05:35 +0300

    Initialize bundled timezone data at startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'src/app/ruh_code_app.dart';
+import 'src/calculation_core/time/time_zone_runtime.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  TimeZoneRuntime.initialize();
   runApp(const RuhCodeApp());
 }
