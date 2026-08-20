4179957588c40e72780719242d60a86bee4a1fa1
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-08-20 20:59:26 +0300

    Initialize production app runtime before launch

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'src/app/app_runtime.dart';
 import 'src/app/ruh_code_app.dart';
 import 'src/calculation_core/time/time_zone_runtime.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   TimeZoneRuntime.initialize();
-  runApp(const RuhCodeApp());
+  final runtime = await RuhCodeRuntime.create();
+  runApp(RuhCodeApp(featureAccess: runtime.featureAccess));
 }
