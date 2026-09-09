153f3759c0441688db47b905dc268dc98ffb183e
isarpsiyahofficial <isarpsiyahofficial@gmail.com>
2026-09-09 16:56:39 +0300

    fix(ci): correct spiritual journal package import

--- test/application/spiritual_journal_core_rc0224_rc0229_test.dart
+++ test/application/spiritual_journal_core_rc0224_rc0229_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ruh_code_app/src/application/spiritual/spiritual_journal_core.dart';
+import 'package:ruh_code/src/application/spiritual/spiritual_journal_core.dart';
 
 void main() {
   final created = DateTime.utc(2026, 9, 7, 8);
